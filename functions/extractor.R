40e391294d22d42c071953fedb464b18f3e3f4cd
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-02-21 15:43:04 +1100

    Allowing those that have written F_{1}_{2} = \\d, etc. to be counted (i.e., no brackets around dfs, which some people seem to do). fixing chi square up to allow more spaces. Changing partial eta squared to make it doesn't capture just the eta

--- functions/extractor.R
+++ functions/extractor.R
@@ -1,18 +1,20 @@
 # F and T test patterns -  
 patternT <- "\\bt\\s*\\(\\s*\\d{1,}\\.?\\d*\\s*\\)\\s*=\\s*-?\\s*\\d*\\.?\\d*(\\s*,?\\s*p\\s*[<>=]\\s*\\d?\\.\\d+e?-?\\d*)?"
-patternF <-  "\\bF\\s*\\(\\s*\\d{1,},\\s*\\d{1,}\\s*\\)\\s*=\\s*\\d*\\.?\\d*(\\s*,?\\s*p\\s*[<>=]\\s*\\d?\\.\\d+e?-?\\d*)?"
+patternF <-  "\\bF\\s*\\(?\\s*\\d{1,},\\s*\\d{1,}\\s*\\)?\\s*=\\s*\\d*\\.?\\d*(\\s*,?\\s*p\\s*[<>=]\\s*\\d?\\.\\d+e?-?\\d*)?"
 patternR <-  "\\b(((r(pb)?(?!2)\\s*\\(?\\s*(df|n)?\\s*[=]?\\s*\\d{0,10}\\s*\\)?\\s*[=]\\s*)|((correlation)\\s*(coefficient)?\\s*([=]|(of))\\s*))(\u2212?\\-?\\s?\\d*\\.?\\d{1,}))(\\s*,?\\s*p\\s*[<>=]\\s*\\d?\\.\\d+e?-?\\d*)?"
+patternChiSq <- "\\b(chi|\\u03C7|\\u1D712|\\u1D61|\\u1D6a|\\u1D712|\\u1D74C|\\u1D86|\\u1D7C0)\\s*(2|square|squared)?\\s*\\(?\\s*(df|n)?\\s*[=]?\\s*\\d*\\s*\\,?\\s*(df|n)?\\s*\\=?\\s*\\d*\\s*\\)?\\s*([=]|(of))\\s*-?\\s*\\d*\\.?\\d*(\\s*,?\\s*p\\s*[<>=]\\s*\\d?\\.\\d+e?-?\\d*)?"
 patternD <- "\\b[dg]\\s*[\\.\\,\\:\\;]?\\s*([=]|(of))\\s*(\\-?\\s*\\d*\\.?\\d{1,})"
-patternEta <- "\\b([\u03B7]\\s*p?\\s*2?\\s*)|((partial)?\\s*eta\\s*squared)\\s*([=]|(of))\\s*(\\-?\\s*\\d*\\.?\\d{1,})" 
+patternEta <- "\\b([(partial\\s*)?\u03B7|(eta)]\\s*p?\\s*2?(squared)?\\s*)\\s*([=]|(of))\\s*(\\-?\\s*\\d*\\.?\\d{1,})" 
 patternHR <- "\\b((HR)|(hazzard.{1,3}?ratio))\\s*((of)|(=))\\s*(\\-?\\s*\\d*\\.?\\d{1,})"
 patternOR <- "\\b((OR)|(odd.{1,3}?ratio))\\s*((of)|(=))\\s*(\\-?\\s*\\d*\\.?\\d{1,})"
 
 # Do not change order of patterns, that will break everything 
 patterns <- c(patternT, patternF, patternR, patternChiSq, patternD, patternEta, patternHR , patternOR)
 
+str_extract_all( resultsText, patterns)
 # helper function to split the output into constituate parts 
 splitTestStatToDF <- function(statistic, cleanedTestStat) {
-  if (length(cleanedTestStat > 0)) {
+  if ((length(cleanedTestStat) > 0) & (length(statistic) > 0)) {
     testStatistic <-
       str_extract(str_split(cleanedTestStat, "=", simplify = T)[, 2],
                   "-?\\d*\\.?\\d*")
@@ -63,7 +65,7 @@ addContext <- function(extracted, contextSize) {
 # function to extract text, remove whitespaces and unicode encodings of the minus sign and return test statistic original data plus df
 extractTestStats <- function(inputText, context = FALSE, contextSize = 100) {
  
-  if(is_empty(inputText)) { 
+  if(is_empty(inputText) | !elementExists(inputText)) { 
     if(context == F) { 
       return(data_frame(statistic = NA, cleaned = NA, reported = NA, value = NA,  df1 = NA, df2  = NA, p = NA))
     } else {
@@ -87,7 +89,7 @@ extractTestStats <- function(inputText, context = FALSE, contextSize = 100) {
     patternsContext <- lapply(extracted, addContext, contextSize = contextSize)
     # extracting discovered hits + context 
     extractedContext <- lapply(patternsContext, function(patternsContext)  
-      unlist(str_extract_all(inputText, regex(as.character(patternsContext), ignore_case = T))))
+      unlist(str_extract(inputText, regex(as.character(patternsContext), ignore_case = T))))
     # returning all of this 
     statisticalOutput <- rbind(data_frame(statistic = "t",   cleaned = extractedClean[[1]], reported = extracted[[1]], context = extractedContext[[1]]),
                  data_frame(statistic = "F",   cleaned = extractedClean[[2]], reported = extracted[[2]],  context = extractedContext[[2]]),
@@ -108,14 +110,14 @@ extractTestStats <- function(inputText, context = FALSE, contextSize = 100) {
                  data_frame(statistic = "HR",  cleaned = extractedClean[[7]], reported = extracted[[7]]),
                  data_frame(statistic = "OR",  cleaned = extractedClean[[8]], reported = extracted[[8]]))
     }
- if(is_empty(statisticalOutput[[1]])) { 
+ if(is_empty(statisticalOutput[[1]]) |  (nrow(statisticalOutput) < 1)) { 
    if(context == F) { 
      data_frame(statistic = NA, cleaned = NA, reported = NA, value = NA,  df1 = NA, df2  = NA, p = NA) 
      } else {
      data_frame(statistic = NA, cleaned = NA, reported = NA, context = NA, value = NA,  df1 = NA, df2  = NA, p = NA)
      }
    } else {
-   cbind(statisticalOutput, splitTestStatToDF(statistic = statisticalOutput$statistic, statisticalOutput$cleaned),stringsAsFactors = F)
+   cbind(statisticalOutput, splitTestStatToDF(statistic = statisticalOutput$statistic, cleanedTestStat = statisticalOutput$cleaned), stringsAsFactors = F)
   }
 }
 
