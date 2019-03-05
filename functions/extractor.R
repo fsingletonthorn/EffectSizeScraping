7254ff5e3d04f6fa81d9a317e4e493af1da9750b
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-03-05 13:15:21 +1100

    Adding in some notes for how to avoid MSE reported alongside F statistics

--- functions/extractor.R
+++ functions/extractor.R
@@ -12,6 +12,8 @@ patternOR <- "\\b((OR)|(odd.{1,3}?ratio))\\s*((of)|(=))\\s*(\\-?\\s*\\d*\\.?\\d{
 patterns <- c(patternT, patternF, patternR, patternChiSq, patternD, patternEta, patternHR , patternOR)
 
 
+# This is a test of one that does not capture MSEs, actually does not work because of the way that the context capture works 
+str_extract("F(1,3) = 1232.12, MSE = 1232, p = 1.1232", "\\bF\\s*\\(?\\s*\\d{1,},\\s*\\d{1,}\\s*\\)?(?:\\,\\s{0,5}MSE\\s{0,5}\\,\\s{0,5}\\=\\d{1,}\\.?\\d{1,}\\,)?\\s*=\\s*\\d*\\.?\\d*(\\s*,?\\s*p\\s*[<>=]\\s*\\d?\\.\\d+e?-?\\d*)?")
 
 
 str_extract_all( resultsText, patterns)
