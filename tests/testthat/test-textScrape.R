27ac989bfc93caab4781fc62aa32ee343d090ff1
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-03-09 17:36:28 +1100

    Adding a test for the scraper

---
+++ tests/testthat/test-textScrape.R
@@ -0,0 +1,14 @@
+context("Scraping functions")
+suppressWarnings( library(stringr))
+suppressWarnings(library(stringi))
+suppressWarnings(library(pdftools))
+
+
+
+test_that("extractPdf extracts correctly", {
+pdf <- extractPdf("https://osf.io/nztsx/download")
+pdf2 <- extractPdf("https://osf.io/nztsx/download")
+
+ expect_true(str_count( pdf[[1]], "or") == 29)
+ expect_true(length(pdf) == 10)
+})
