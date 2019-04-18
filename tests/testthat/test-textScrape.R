6cdabe6a51cbb838f0ae121fd79b1bbbfcbc01e7
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-04-19 09:10:43 +1000

    Fixing tests

--- tests/testthat/test-textScrape.R
+++ tests/testthat/test-textScrape.R
@@ -1,11 +1,20 @@
 context("Scraping functions")
 
 test_that("extractPdf extracts all pages", {
-pdf <- extractPdf("https://osf.io/nztsx/download")
-pdf2 <- extractPdf("https://osf.io/nztsx/download")
-
- expect_true(stringr::str_count( pdf[[1]], "or") == 29)
- expect_true(length(pdf) == 10)
+  pdf <- extractPdf("https://osf.io/nztsx/download")
+  # pdf2 <- extractPdf("https://osf.io/kcbnu/download")
+  # expect_true(sum(lapply(pdf$text , stringr::str_count, pattern = "or")))
+  expect_true(
+    stringr::str_detect(
+      pdf[[5, 2]],
+      stringr::fixed(
+        "participants may have given inaccurate or exaggerated responses due to poor awareness and/or social desirability bias"
+      )
+    ) & stringr::str_detect(
+        pdf[[5, 2]],
+        stringr::fixed("In this experimental study, we manipulated")
+      )
+  )
 })
 
 ## Need to test PDF version too
