456d4eebbde59cf15dc02899d9872a22727dd3c0
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-04-23 13:27:00 +1000

    Function now looks for pdf if the pmc article does not arrive in xml - you need to supply the tar location

--- R/pmc_scrape.R
+++ R/pmc_scrape.R
@@ -2,21 +2,64 @@
 # Download article from PMC
 
 # Pull PMC
-# pullPMC()
 
+# articles[articles$PMID == ]
 
+# Example sans text 
+# pmcID <- 4710337
+# call <- articles$oaiCall[articles$PMCID == pmcID]
+# ftpCall  <- articles$tpfCall[articles$PMCID == pmcID]
 
-# If there is no text 
-if(T) {
-  # Download from FTC
+scrapePMC <- function(call, ftpCall, statcheck = F) {
+  pulledPMC <-
+    pullPMC(call = articles$oaiCall[articles$PMCID == pmcID])
   
-  
-  # Find PDF 
-  
-  # Read PDF 
-  
-  # Add PMC id to file here
+  #
+  if ((length(pulledPMC$text) == 2)) {
+    # To check if abstract is NA  add "& is.na(textTemp$text[2,2][[1]])"
+    # articles$tpfCall[articles$PMCID == pmcID]
+    
+    # Create temp file location
+    tempLoc <- tempfile()
+    # download from ftp file 
+    download.file(ftpCall, destfile = tempLoc, quiet = T)
+    # (ftpCall, httr::write_disk(tempLoc, overwrite = T))
+    # Setting extraction directory
+    exdir <- paste0(tempLoc, "_extracted")
+    # Extracting file
+    untar(tempLoc, exdir = exdir)
+    # Finding the PDF file
+    pdfLoc <-
+      list.files(
+        exdir,
+        recursive = T,
+        full.names = T,
+        pattern = ".*\\.pdf"
+      )
+    
+    # If PDF file exists
+    if (length(pdfLoc > 0)) {
+      # Always only work on the first PDF, just in case there is more than 1
+      # This also binds in the PMC number, although it excludes
+      pulledPMC$text <-
+        rbind(pulledPMC$text,  extractPdf(pdfLoc[[1]]))
+      
+      pulledPMC$text <-  processPMC(pulledPMC$text)
+      
+      # Cleanup
+      unlink( c( exdir, tempLoc ), recursive = T)
+      
+      return(pulledPMC)
+      
+    } else {
+      pulledPMC$text <- processPMC(pulledPMC$text)
+      return(pulledPMC)
+    }
+    
+  }
 }
+
+
 
 # Process text file
 # processPMC()
