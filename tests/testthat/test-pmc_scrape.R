356cbf2c437c6a61c47019aa6d944a7672b461b1
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-04-23 15:30:49 +1000

    test_that(scrapePMC extracts files with correctly  and doesn't get any false positives from PMC5393010

--- tests/testthat/test-pmc_scrape.R
+++ tests/testthat/test-pmc_scrape.R
@@ -12,16 +12,35 @@ test_that("extraction works from pdf", {
 })
 
 test_that("scrapePMC extracts files with correctly labeled sections", {
-  pmcID <- 4710337
-  call <- articles$oaiCall[articles$PMCID == pmcID]
-  ftpCall  <- articles$tpfCall[articles$PMCID == pmcID]
+  # pmcID <- 4710337
+  call <- "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:4710337&metadataPrefix=pmc"
+  ftpCall  <- "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_package/bd/41/PMC4710337.tar.gz"
   output <- scrapePMC(call, ftpCall, statcheck = F)
   
   expect_true(
-  str_detect(output$text$text[output$text$names == "unlabelled"],
+  stringr::str_detect(output$text$text[output$text$names == "unlabelled"],
              "Prompt diagnosis is important as urgent")
   )
   expect_true( is.na(output$text$statisticalOutput) )
 })
+
+
+
+test_that("scrapePMC extracts files with correctly labeled sections", {
+  pmcID <- 5393010
+  call <- "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:5393010&metadataPrefix=pmc"
+  ftpCall  <-"ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_package/a2/4e/PMC5393010.tar.gz"
+  output <- scrapePMC(call, ftpCall, statcheck = F)
+  
+  expect_true(
+    stringr::str_detect(output$text$text[output$text$names == "unlabelled"],
+                        "Prompt diagnosis is important as urgent")
+  )
+  expect_true( is.na(output$text$statisticalOutput) )
+})
+
+
+
+
 
 
