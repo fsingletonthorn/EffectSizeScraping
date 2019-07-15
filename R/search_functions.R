5b760ce3805ea1d7ecd0c7b8cc6614d3f561c97c
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-07-15 18:01:28 +0200

    Refactoring for clarity

--- R/search_functions.R
+++ R/search_functions.R
@@ -1,49 +1,51 @@
 # Functions that search text for CIs, Power analysis, that attempt to extract sample sizes
 
 
+
 # Check for 95% CIs
 checkCIs <- function(input,
                      context = T,
                      contextSize = 100) {
+  ciRegex  <- "((CI|Confidence Interval)\\s\\[\\d*,\\s*\\d*\\])"
   
-CIreg <-
-  paste(
-    paste0(".{0,", contextSize, "}((\\b\\d{1,2}%\\s*CI\\b)"),
-    "((CI|Confidence Interval)\\s\\[\\d*,\\s*\\d*\\])",
-    paste0("(confidence\\s*interval)).{0,", contextSize, "}"),
-    sep = "|",
-    collapse = "|"
-  )
-
-CIs <- stringr::str_extract_all(input,
-                                stringr::regex(CIreg,
-                                               ignore_case = T))
-
-CIsBinary <- stringr::str_detect(input,
-                                 stringr::regex(CIreg,
-                                                ignore_case = T))
-if (context == T) {
-  return(list(CIs, CIsBinary))
-} else {
-  return(CIsBinary)
+  CIreg <-
+    paste(
+      paste0(".{0,", contextSize, "}((\\b\\d{1,2}%\\s*CI\\b)"),
+      ciRegex,
+      paste0("(confidence\\s*interval)).{0,", contextSize, "}"),
+      sep = "|",
+      collapse = "|"
+    )
+  
+  CIs <- stringr::str_extract_all(input,
+                                  stringr::regex(CIreg,
+                                                 ignore_case = T))
+  
+  CIsBinary <- stringr::str_detect(input,
+                                   stringr::regex(CIreg,
+                                                  ignore_case = T))
+  if (context == T) {
+    return(list(CIs, CIsBinary))
+  } else {
+    return(CIsBinary)
+  }
 }
-}
 
-
-
-
+### This does not work yet be careful
 findN <- function(input,
                   context = T,
                   contextSize = 100) {
   
-  findNReg <- 
-  
+  input <- textclean::num
+  findNReg <-  ""
+
   paste(
     paste0(".{0,", contextSize, "}((\\b\\d{1,2}%\\s*CI\\b)"),
-    "((CI|Confidence Interval)\\s\\[\\d*,\\s*\\d*\\])",
+    
     paste0("(confidence\\s*interval)).{0,", contextSize, "}"),
     sep = "|",
     collapse = "|"
   )
 }
+
 
