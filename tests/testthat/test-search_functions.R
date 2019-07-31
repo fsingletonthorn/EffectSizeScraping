ed11a4bcba0cf8ac10b519524140a43861df5884
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-07-31 10:34:25 +1000

    Writing one test and prepping for others of "findN"

--- tests/testthat/test-search_functions.R
+++ tests/testthat/test-search_functions.R
@@ -31,7 +31,25 @@ expect_identical(findN("Twenty Princeton University undergraduate volunteers at 
 expect_identical(findN("Forty Princeton University volunteers at the student campus center read a short review of a new MP3 player,")[[2]][[1]], "40")
 expect_identical(findN("Fifty-one Hebrew University undergraduates participated in the experiment in exchange for course credit or pay.")[[2]][[1]], "51")
 expect_identical(findN("Ninety-three Hebrew University undergraduates participated in exchange for course credit or pay.")[[2]][[1]], "93")
+expect_identical(findN("Ninety-three Hebrew University undergraduates participated in exchange for course credit or pay. 
+                       In experiment 2, 23 Princeton University volunteers at the student campus center read a short review of a new MP3 player,
+                       Experiment 3: 94 participants read the paper. ")$N, matrix(c("93", "23", "94")))
 })
 
 # Does not work: 
 # expect_identical(findN("10 princeton undergraduate students reported")[[2]][[1]], "10")
+
+# Test on real papers
+
+
+# To be added: 
+# test_that("CI extractor works", {
+#   ciTexts <- list("Confidence interval", 
+#                   "as fsad f 95% CI fasd", 
+#                   "95% CI around the latest [1,10]", 
+#                   "95$ confidence range", 
+#                   "not very good at confidence ratings")
+#   
+#   testthat::expect_equivalent(checkCIs(ciTexts)[[1]][c(1,2)], ciTexts[1:2])
+#   testthat::expect_equivalent(checkCIs(ciTexts, context = F), c(T,T,T,F,F))
+# })
