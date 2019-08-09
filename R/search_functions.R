67e8daaae579756a0f090e17602d9ac593961fb2
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-08-09 12:31:59 +1000

    Prototypes and tests for search functions for Ns and for CI binary

--- R/search_functions.R
+++ R/search_functions.R
@@ -23,16 +23,19 @@ checkCIs <- function(input,
   # Extract just CIs
   CIs <- stringr::str_extract_all(CIs_context,
                                   stringr::regex(CI_REGEX,
-                                                 ignore_case = T))
+                                                 ignore_case = T), 
+                                  simplify = T)
   } else {CIs <- character(0)}
   
-  if (context == T) {
-    return(list(CIs, CIs_context, CIsBinary))
+  if (context == T & CIsBinary == T) {
+    return(tibble::tibble(CIs = CIs, context = CIs_context, CIBinary = CIsBinary))
+  } else if (context == T & CIsBinary == F) {
+    tibble::tibble(CIs = NA, context = NA, CIBinary = CIsBinary)
   } else {
-    return(CIsBinary)
+    return(tibble::tibble( CIBinary = CIsBinary) )
   }
 }
-
+ 
 findN <- function(input) {
   
   # Converting words to numbers for sample size extraction
@@ -43,8 +46,8 @@ findN <- function(input) {
   # Setting up regexs
   SSOf <- "(\\ba?\\s*sample\\s*size\\s*of\\s*\\d+)"
   nOf <- "((\\bN\\s*(\\=|(of))\\s*)\\d+)"
-  nPar <- "(\\b\\d+((?!\\.|\\d).)*(participants|participated|volunteer(s|ed)|observations))"
-  parWere <- "(\\b(subjects|participants)((?!\\.|\\d).)*\\s*were((?!\\.|\\d).)*\\d+)"
+  nPar <- "(\\b\\d+((?![:punct:]|\\d).)*(participants|participated|volunteer(s|ed)|observations))"
+  parWere <- "(\\b(subjects|participants)((?!\\[:punct:]|[\\(\\)\\.\\,]).|\\d)*\\s*were((?![:punct:]|\\d).)*\\d+)"
   
   # pullting together regex 
   NRegex <-  paste(SSOf, nOf, nPar, parWere , sep = "|")
@@ -55,13 +58,25 @@ findN <- function(input) {
                                                 simplify = T)
   
   if(elementExists(sampleSizeStrings)) {
-  sampleSizes <- stringr::str_extract_all(sampleSizeStrings, "\\d{1,}", 
-                                          simplify = T)
+  sampleSizes <- stringr::str_extract(sampleSizeStrings, "\\d{1,}")
   } else {sampleSizes <- character(0)}
   
-  return(list(string = sampleSizeStrings, N = sampleSizes))
+  return(tibble::tibble(string = t(sampleSizeStrings), N = sampleSizes))
 }
-# 
+
+# Function to process text output in the categorisation functions of this package
+extract_ns_from_processed <- function(text){
+  # Text must be a tibble / df with column 1 = "names", column 2 = "text"
+dplyr::bind_rows(
+  apply(text, MARGIN = 1, function(x) {
+    name <- x[1]
+    Ns <- findN(x[2])
+    return(suppressWarnings(tibble::tibble(name = rep(name, nrow(Ns)), nString = Ns[[1]], n = Ns[[2]])))
+  }
+  )
+)
+}
+
 # findPowerAnalysis <- {
 #   stringr::str_extract_all()
 #   }
