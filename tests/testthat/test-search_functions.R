8f2f8703dd0d498ba58d6136835d968d89959a86
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-08-19 17:14:20 +1000

    Adding test for NA bug in checkCI

--- tests/testthat/test-search_functions.R
+++ tests/testthat/test-search_functions.R
@@ -47,21 +47,21 @@ test_that("false positive sample sizes are avoided as much as is possible", {
 # Does not work: 
 # expect_identical(findN("10 princeton undergraduate students reported")[[2]][[1]], "10")
 
-# Test on real papers
+# # Test on real papers
 test_that("the correct number is extracted", {
   pdf <- extractPdf("https://osf.io/v7n6j/download")
   expect_true(
     max( as.numeric(
       extractNsFromProcessed( pdf )$n )) == 510
     )
-  
+
   expect_equal(
     findN("participants in some conditions were, indeed, more familiar with the concept than those in other conditions, this could confound results. However, it is likely that greater familiarity would lead to greater ac- ceptance (Bryant & Barnett, 2018), and in this instance, the reverse was true: those claiming to be more familiar in the ‘lab grown meat’ and ‘cultured meat’ conditions actually also showed lower measures of ac- ceptance in subsequent analyses. Therefore, we are confident that this Appetite 137 (2019) 104–113 Items Eating [X] is likely to be healthy. [X] is likely to look, taste, smell, and feel the same as conventional meat. I think I could tell the difference between [X] and conventional meat. [X] is likely to contain chemicals or ingredients which should be avoided. [X] is likely to be safe for human consumption. I would trust [X]. [X] is unnatural. [X] is appealing to me. I feel positive about the development of [X]. The idea of [X] is disgusting. I feel comfortable about the idea of eating [X]. I would be anxious about eating [X]. Eating [X] would conflict with my values. I feel that I would have control over my decision to eat [X] or not. The production of [X] is a necessary scientific development. Others would disapprove of me eating [X]. [X] will have benefits for our society. Production of [X] is wise. Production of [X] is necessary. [X] is more environmentally friendly than conventional meat. Producing [X] poses a risk to society. I would be willing to try [X]. I would buy [X] regularly. I would eat [X] instead of conventional meat. I would rather eat [X] than soy-based meat substitutes or Quorn. I would pay more for [X] than for conventional meat. difference is a result of how familiar the names seem rather than how familiar the participants actually were. Familiarity was therefore not included as a covariate in subsequent analyses. 3.2. Word associations Before a description of IVM had been given, participants completed a word association task. They generated a total 721 words or phrases – where 338 of them were unique - an average of 3.90 per participant. They also rated the valence of each word or phrase they generated. Words were sorted into categories. Initial categories were identified, partly informed by themes observed in the literature on consumer ac- ceptance of IVM. After consultation, these categories were adjusted and some words were reclassified. Next, 3 independent raters allocated the words to categories with an initial agreement rate of 67%, which increased to 97% after further discussion with 1 rater. The remaining 3% of ambiguous words were categorised after further consultation between the co-authors. Words were ultimately placed into 24")$N,
     character(0)
     )
-  
+
   pdf <- extractPdf("https://osf.io/nztsx/download")
-  
+
   expect_true(
     max( as.numeric(
       extractNsFromProcessed( pdf )$n)) == 185
@@ -91,5 +91,10 @@ test_that("CI extractor works II", {
    testthat::expect_equivalent(unlist(lapply(ciTexts, checkCIs, context = F)), c(T,T,T,F,T,F))
  })
 
+test_that("CI extractor does not return True for NA", {
+  expect_false(checkCIs(NA)$CIBinary)
+  }
+)          
+  
 
 
