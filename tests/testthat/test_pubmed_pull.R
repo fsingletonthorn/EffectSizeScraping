0f79a641ad0c3b37906e4cb79a3a7a5ee931df51
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-12-06 17:24:27 +1100

    putting in text for extacting editors info, fixes #57

--- tests/testthat/test_pubmed_pull.R
+++ tests/testthat/test_pubmed_pull.R
@@ -26,3 +26,78 @@ test_that("pubmebPull doesn't bring line breaks with keywords", {
   processedText <- pullPMC(call =  "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:5414266&metadataPrefix=pmc")
   expect_false(any(stringr::str_detect(processedText$keywords$keywords, "\\n")))
   })
+
+# Editor checks:
+#Plos one edi
+test_that("pullPMC can find editors", {
+  output <- pullPMC(call = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:4648576&metadataPrefix=pmc") 
+  expect_equal(dim( output$editors), c(1,3) )
+  expect_equal( output$editors$surname, "Voracek" )
+  expect_equal( output$editors$firstname, "Martin" )
+}
+)
+
+# Frontiers in Behaviorual neuroscience
+test_that("pullPMC can find editors", {
+output <- pullPMC(call = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:6379263&metadataPrefix=pmc")
+expect_equal(dim( output$editors), c(0,3) )
+expect_true( stringr::str_detect(output$editors_unstructured[1,2], "Antonella Gasbarri"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Nadja Schroder"))
+}
+)
+
+# Frontiers in neuroscience
+test_that("pullPMC can find editors", {
+output <- pullPMC(call = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:6018545&metadataPrefix=pmc")
+expect_equal(dim( output$editors), c(0,3) )
+expect_true( stringr::str_detect(output$editors_unstructured[1,2], "Graziella Madeo"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Paolo Gubellini"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Nicolas Xavier Tritsch"))
+}
+)
+
+# Frontiers in Human Neuroscience
+test_that("pullPMC can find editors", {
+output <- pullPMC(call = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:3882720&metadataPrefix=pmc")
+expect_equal(dim( output$editors), c(0,3) )
+expect_true( stringr::str_detect(output$editors_unstructured[1,2], "Edited by: Merim Bilalic, University Tübingen"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Reviewed by: E. Glenn Schellenberg, University of Toronto, Canada"))
+}
+)
+
+# "Frontiers in Psychiatry"
+test_that("pullPMC can find editors", {
+output <- pullPMC(call = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:4033190&metadataPrefix=pmc")
+expect_equal(dim( output$editors), c(0,3) )
+expect_true( stringr::str_detect(output$editors_unstructured[1,2], "Edited by: Elizabeth Clare Temple, Federation University Australia, Australia"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Reviewed by: Thomas Hillemacher, Hannover Medical School"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Elizabeth Clare Temple, Federation University Australia"))
+}
+)
+
+# Frontiers in Human Neuroscience
+test_that("pullPMC can find editors", {
+output <- pullPMC(call = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:3882720&metadataPrefix=pmc")
+expect_equal(dim( output$editors), c(0,3) )
+expect_true( stringr::str_detect(output$editors_unstructured[1,2], "Edited by: Merim Bilalic, University Tübingen"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Reviewed by: E. Glenn Schellenberg, University of Toronto, Canada"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Reviewed by: E. Glenn Schellenberg, University of Toronto, Canada"))
+}
+)
+
+# Frontiers in psychology example
+test_that("pullPMC can find editors", {
+output <- pullPMC(call = "https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:6543906&metadataPrefix=pmc")
+expect_equal(dim( output$editors), c(0,3) )
+expect_true( stringr::str_detect(output$editors_unstructured[1,2], "Edited by:"))
+expect_true( stringr::str_detect(output$editors_unstructured[1,2], "Michele Biasutti, University of Padova, Italy"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Reviewed by:"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Catherine Alexandra Lebel"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "Floris Tijmen Van Vugt"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "University of Calgary, Canada"))
+expect_true( stringr::str_detect(output$editors_unstructured[2,2], "McGill University, Canada"))
+}
+)
+
+
+
