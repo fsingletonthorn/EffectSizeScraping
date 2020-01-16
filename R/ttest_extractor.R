56dd044e0acb5182feac2ea6cc2e376339732739
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2020-01-17 10:14:51 +1100

    Functional t test extractor with a reasonably full test set

--- R/ttest_extractor.R
+++ R/ttest_extractor.R
@@ -16,19 +16,19 @@ extractTTests <- function(input) {
   # Setting up all possible parts of the regex
   ttestRegex  <- "(\\bt\\b)"
   
-  numbericRegex_commas <- "(((\\d{1,3}(?=,)(,\\d{3})*)|\\d+)?)"
+  numbericRegex_commas <- "(((\\d{1,3}(?=,)(,\\d{3}){0,99})|\\d{1,99})?)"
   
-  numbericRegex_commasdecimals <- "(((\\d{1,3}(?=,)(,\\d{3})*)|\\d+)(\\.\\d+)?)"
+  numbericRegex_commas_decimals <- "((((\\d{1,3}(?=,)(,\\d{3}){0,99})|\\d{1,99})(\\.\\d{1,99})?)|(\\.\\d{1,99}))"
   
-  degreesOfFreedomRegex_commas <- paste0("((?i)\\(?\\s*((df\\s*\\=?\\s*)|(n\\s*\\=\\s*))?\\s*",
-                                         numbericRegex_commas,
-                                         "\\s*\\)?(?-i))")
+  degreesOfFreedomRegex_commas_decimals <- paste0("((?i)\\(?\\s{0,5}((df\\s{0,5}\\=?\\s{0,5})|(n\\s{0,5}\\=\\s{0,5}))?\\s{0,5}",
+                                                  numbericRegex_commas_decimals,
+                                         "\\s{0,5}\\)?(?-i))")
   ofOrEqualsRegex <- "((of)|=|:)"
   
-  numbericBelow1Regex <- "((?<![1-9])\\.\\d+|0(\\.\\d+)?|(1\\.0*(?!(0*[1-9])))|((?<![0-9\\.])1(?![\\.0-9])))"
+  numbericBelow1Regex <- "((?<![1-9])\\.\\d{1,99}|0(\\.\\d{1,99})?|(1\\.0{0,99}(?!(0{0,99}[1-9])))|((?<![0-9\\.])1(?![\\.0-9])))"
   # additional p value detector
   # additional p value detector
-  pValueRegex <- "((?i)((\\s*,?\\s*)(ns))|(p\\s*[<>=(ns)]\\s*((ns)|(\\d?\\.?\\d+e?-?\\d*)|(\\.\\d+)))(?-i))"
+  pValueRegex <- "((?i)((\\s{0,5},?\\s{0,5})(ns))|(p\\s{0,5}[<>=(ns)]\\s{0,5}((ns)|(\\d?\\.?\\d{1,99}e?-?\\d{0,99})|(\\.\\d{1,99})))(?-i))"
   
   # Additional df = detector
   # Additional n = detector
@@ -38,21 +38,30 @@ extractTTests <- function(input) {
     ttestRegex,
     # Allowing spaces
     "\\s{0,5}",
-    degreesOfFreedomRegex_commas,
+    degreesOfFreedomRegex_commas_decimals,
     # Making degrees of freedom optional
     "?",
     # Allowing spaces
-    "\\s*",
+    "\\s{0,5}",
     ofOrEqualsRegex,
     # Allowing spaces or negatives
-    "\\s*",
+    "\\s{0,5}",
     "\\-?",
-    "\\s*",
-    numbericRegex_commasdecimals,
-    "\\s*",
+    "\\s{0,5}",
+    numbericRegex_commas_decimals,
+    # Allowing spaces
+    "\\s{0,5}",
     "\\;?",
     ",?",
-    "\\s*",
+    "\\s{0,5}",
+    paste0("((?i)df\\s*[\\=\\:\\;]\\s*", 
+           numbericRegex_commas_decimals,
+           "(?-i))?"
+    ),
+    "\\s{0,5}",
+    "\\;?",
+    ",?",
+    "\\s{0,5}",
     pValueRegex, 
     "?" # Making p values optional
   ) 
@@ -71,25 +80,27 @@ extractTTests <- function(input) {
     paste0(
       ttestRegex,
       # Allowing spaces
-      "\\s*",
-      degreesOfFreedomRegex_commas,
+      "\\s{0,5}",
+      degreesOfFreedomRegex_commas_decimals,
       # Making degrees of freedom optional
       "?",
       # Allowing spaces
-      "\\s*",
+      "\\s{0,5}",
       ofOrEqualsRegex,
       # Allowing spaces or negatives
-      "\\s*"))
+      "\\s{0,5}"))
   
   
-  value <- stringr::str_extract(
+  value <- 
+    stringr::str_remove_all(
+    stringr::str_extract(
     value_with_p, 
     paste0("^",
-           "(\\s*",
+           "(\\s{0,5}",
            "\\-?",
-           "\\s*",
-           numbericBelow1Regex, ")")
-  )
+           "\\s{0,5}",
+           numbericRegex_commas_decimals, ")")
+  ), "\\s*")
   
   ps <-
     stringr::str_trim(
@@ -101,7 +112,7 @@ extractTTests <- function(input) {
   cors_with_ns <- 
     stringr::str_detect(
       detected_ttests, 
-      "((?!=[nt])n\\s*\\=\\s*)"
+      "((?!=[nt])n\\s{0,5}\\=\\s{0,5})"
     )
   
   df2 <- 
@@ -119,8 +130,15 @@ extractTTests <- function(input) {
       stringr::regex("[^0-9.]")
     )
   
-  df2 <- ifelse(cors_with_ns, 
-                as.numeric(df2)-2,
+  
+  df2 <- ifelse(df2 == "", stringr::str_remove_all(
+                stringr::str_extract(detected_ttests,
+                  paste0("(?i)df\\s*[\\=\\:\\;]\\s*", 
+                         numbericRegex_commas_decimals,
+                         "(?-i)"
+                         )
+                  ),
+                stringr::regex("[^0-9.]")), 
                 df2
   )
   
--- tests/testthat/test-ttest_extractor.R
+++ tests/testthat/test-ttest_extractor.R
@@ -3,16 +3,119 @@ context("t test extractor")
 
 
 
-extractTTests("t(22) = 2.202")
 
 
+test_that("t test works for simple cases" , {
+
+  expect_true(
+extractTTests("t(22) = 2.202")$df2 == 22  
+  )
+expect_true(
+extractTTests("t(22) = 2.202")$value == 2.202
+)
+
+expect_true(
+extractTTests("t(22) = 2.202, p < .05")$p == "p < .05"
+)
+
+})
 
 
+test_that("t test works for slightly aborant cases" , {
+
+expect_true(
+extractTTests("t = 2.202, df = 22")$df2 == 22  
+)
+  
+expect_true(
+extractTTests("t = 2.202, df = 22")$value == 2.202
+)
+
+expect_true(
+extractTTests("t = 2.202, df = 22, p < .05")$p == "p < .05"
+)
+
+})
 
 
+testT <- c("t(1) = 12.42345",
+           "t(2)=12.42345",
+           "t(3) = 12.42345",
+           "t(4) = 12.42345",
+           "t(5 ) = 1",
+           "t(6 ) = .42345",
+           "t(7 ) = 12",
+           "t(8) = -12.42345",
+           "t(9)=-12.42345",
+           "t(10) = -12.42345",
+           "t(11) = -12.42345",
+           "t(12 ) = -1",
+           "t(13 ) = -.42345",
+           "t(14 ) = -12",
+           "t(15) = - 12.42345",
+           "t(16)=- 12.42345",
+           "t(17) = - 12.42345, p = .123",
+           "t(18) = - 12.42345",
+           "t(19 ) = - 1",
+           "t(20 ) = - .42345, p = 0.00121",
+           "t(21 ) = - 12",
+           "t(22) = 12.42345",
+           "t (23)=12.42345",
+           "t (24) = 12.42345",
+           "t (25) = 12.42345",
+           "t (26 ) = 1",
+           "t (27 ) = .42345",
+           "t (28 ) = 12",
+           "t (29 ) = 12.0, p = .99",
+           "t (30 ) = 12.0, P = .99",
+           "t = 12.0, df = 31, P < 1e-07",
+           "t = 12.0, df = 32, P > .01",
+           "t = 12.0, df = 33,  p > .05",
+           "t = 12.0, p = .99",
+           "t = 12.0, P = 0.99",
+           "t = 12.0, P = 0.01",
+           "t = 12.0, P = 0.001",
+           "t = 12.0, P < 1e-07",
+           "t = 12.0, P > .01"
+           )
+
+testTString  <- stringr::str_flatten(testT, collapse = " ")
 
 
+test_that("t test extractor works", {
+  extracted <- extractTTests(testTString)
+  
+  expect_identical(extracted$raw, testT)
 
+  expect_identical(extracted$value,
+                   stringr::str_remove_all(
+                     stringr::str_extract(
+                       testT,
+                       "(?<=t\\s{0,3}\\(?\\s{0,3}\\d{0,10}\\s{0,3}\\)?\\s{0,3}\\=\\s{0,3})\\s{0,3}-?\\s{0,3}\\d*\\.?\\d*"
+                     ),
+                     "\\s"
+                   ))
+  
+  expect_true(all(is.na(extracted$df1)))
+  
+  expect_identical(extracted$df2[1:33], as.character(1:33)
+  )  
+  
+  
+ # expect_true(all(is.na(extracted$p[31:32])))
+  
+  expect_identical(extracted$p,
+                     stringr::str_extract(
+                       testT,
+                       "(p|P).*"
+                     )
+                   )
+  })
 
-
+test_that("t test doesn't pick up tests without reported values", {
+  
+  expect_equal(
+    dim(extractTTests("t(1), p = .8, and t(c) = .12")), c(0, 6))
+  
+  })
 
